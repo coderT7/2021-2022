afe0b46b47c18a497272adde89bd2af4242e58f9
CoderXt <724435851@qq.com>
2022-01-05 20:21:48 +0800

    修复了erase重复删除会造成内存错误的bug

--- list/text.c
+++ list/text.c
@@ -5,15 +5,20 @@ int main()
     ListNode *phead = ListInit();
     push_back(phead,10);
     push_back(phead,10);
+    push_back(phead,10);
+    push_front(phead,20);
     push_front(phead,20);
     push_front(phead,20);
     pop_back(phead);
+    pop_back(phead);
+    pop_back(phead);
+    pop_front(phead);
     pop_front(phead);
     ListNode *pos = find(phead,20);
     insert(pos,40);
     insert(pos,80);
-    erase(pos);
-    clean(&phead);
+    erase(&pos);
+    erase(&pos);
     print_list(phead);
     return 0;
 }
