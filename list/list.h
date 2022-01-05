6cc71148a471162efd7e28f1b8bee7184db2ce06
CoderXt <724435851@qq.com>
2022-01-05 18:53:13 +0800

    重新写的双向带头循环链表代码

---
+++ list/list.h
@@ -0,0 +1,38 @@
+#pragma once 
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+typedef int LTDatatype;
+
+typedef struct ListNode
+{
+    struct ListNode *prev;
+    struct ListNode *next;
+    LTDatatype val;
+}ListNode;
+
+ListNode* ListInit();
+
+void print_list(ListNode *phead);
+
+void push_back(ListNode *phead,LTDatatype x);
+
+void push_front(ListNode *phead,LTDatatype x);
+
+void pop_back(ListNode *phead);
+
+void pop_front(ListNode *phead);
+
+ListNode* find(ListNode *phead,LTDatatype x);
+
+void insert(ListNode *pos,LTDatatype x);
+
+void erase(ListNode *pos);
+
+void clean(ListNode **phead);
+
+
+
---
+++ list/text.c
@@ -0,0 +1,19 @@
+#include"list.h"
+
+int main()
+{
+    ListNode *phead = ListInit();
+    push_back(phead,10);
+    push_back(phead,10);
+    push_front(phead,20);
+    push_front(phead,20);
+    pop_back(phead);
+    pop_front(phead);
+    ListNode *pos = find(phead,20);
+    insert(pos,40);
+    insert(pos,80);
+    erase(pos);
+    clean(&phead);
+    print_list(phead);
+    return 0;
+}
