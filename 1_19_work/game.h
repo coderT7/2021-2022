d796d69789d5ee6dc32169fe5082a1dba11e58a5
CxtCSDN <92586914+CxtCSDN@users.noreply.github.com>
2022-01-20 15:31:58 +0800

    比特作业（1.10-1.19）的源代码

---
+++ 1_19_work/game.h
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<stdio.h>
+#include<time.h>
+#include<Windows.h>
+#include<stdlib.h>
+
+using namespace std;
+
+#define NUM 101 
+
+void game();
