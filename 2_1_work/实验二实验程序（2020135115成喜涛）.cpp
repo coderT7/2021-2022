67003f392b68ec86c65caf4698a0f50e80455ae9
CxtCSDN <724435851@qq.com>
2022-02-01 22:31:01 +0800

    2_1_CodeExercises

---
+++ 2_1_work/实验二实验程序（2020135115成喜涛）.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<iostream>
+//#include<cstdio>
+//#include<ctype.h>
+//#include<cmath>
+//using namespace std;
+//
+///*
+//一、编写程序，格式化输出
+//二、编写程序，实现小写字母转大写。
+//三、编写程序，实现输入两个不同类型数据后，经过适当的运算（加、减、乘、除）后输出。
+//四、编写程序，计算三角形面积、立方体的体积和表面积。
+//五、编写程序，验证int、char、float、double、long int、short int等数据类型的长度。
+//六、编写程序，验证取模运算中，出现负数时的运算规律。*/
+//
+//int main()
+//{
+//	//一、
+//	//printf("%-8s%-8s%-8s%-8s%-8s%-8s\n", "学号", "姓名", "语文", "数学", "英语", "平均分");
+//	//printf("%-8s%-8s%-8s%-8s%-8s%-8s\n", "====", "====", "====", "====", "====", "====");
+//	//printf("%-8s%-8s%-8s%-8s%-8s%-8s\n", "1", "34", "56.0", "778.0", "23.0", "285.7");
+//	//二、
+//	//int ch = getchar();
+//	//if (islower(ch))
+//	//	ch = toupper(ch);
+//	//putchar(ch);
+//	//三、
+//	//int a = 0; float b = 0.0;
+//	//cin >> a >> b;
+//	//cout << a + b << " " << a - b << " " << a * b << " " << a / b << endl;
+//	//四、
+//	//cout << "请输入三角形的三边之长：";
+//	//float a, b, c;
+//	//cin >> a >> b >> c;
+//	//float p = (a + b + c) / 2;
+//	//cout << p * (p - a) * (p - b) * (p - c) << endl;
+//	cout << "请输入立方体的边长：";
+//	float a;
+//	cin >> a;
+//	cout << "立方体的体积为：" << pow(a, 3) << endl << "立方体的表面积为：" << a * a * 6 << endl;
+//	//五、
+//	//cout << sizeof(int) << " " << sizeof(char) << " "
+//	//	<< sizeof(float) << " " << sizeof(double) << " "
+//	//	<< sizeof(long) << " " << sizeof(short) << endl;
+//	//六、
+//	//cout << -2 % 1 << " " << 11 % -5 << " " << - 11 % 5 << " " << -2 % -1 << -11 % -5 << endl;
+//	//由输出结果0 1 -1 0 -1可知，负数进行取模时，若除数与被除数能整除则一样为0，否则结果仍旧是余数
+//	//即取模运算的结果的符号与第一个操作数的符号相同(或为0)
+//	return 0;
+//}
