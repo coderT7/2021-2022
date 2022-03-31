7b506aa87a847c78a30108520e8cbdbb67e18867
CxtCSDN <724435851@qq.com>
2022-03-31 13:37:27 +0800

    数据结构实验2完善

--- 3_11_数据结构实验1/main.cpp
+++ 3_11_数据结构实验1/main.cpp
@@ -1,6 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include"text.h"
+#include"main.h"
 
+//当前版本每次只能处理一个文件的信息，但可以多次进行（每次获得的文件信息仅针对当次处理的文件）
+//扩展：可处理多个文件信息
+//扩展思路一：将输出的文件名定义成随机生成，每次处理完毕时提示用户保存至了哪个文件
+//扩展思路二：利用多态，本质是思路一，但优点在于代码延展性好，维护容易
 int main()
 {
 	int input = 0;
@@ -25,12 +29,14 @@ int main()
 			cout << "请输入要进行超链接删除的文件名：";
 			cin >> str;
 			//hyperlink_delete(str.c_str());
-			blank_line(str.c_str());
+			delete_all(str.c_str());
 			system("pause");
 		}
 			break;
 		case getWord:
 		{
+			//单词文件是以追加方式进行的，为了提高复用性，每次获取前先将原有文件删除
+			remove("word.txt");
 			cout << "请输入要获取单词列表的文件名：";
 			cin >> str;
 			int ret = get_word_num(str.c_str());
@@ -48,6 +54,8 @@ int main()
 		break;
 		case delDup:
 		{
+			//去重同统计单词同理
+			remove("del_word.txt");
 			cout << "请输入要进行单词去重的文件名：";
 			cin >> str;
 			remove_duplicates(str.c_str());
