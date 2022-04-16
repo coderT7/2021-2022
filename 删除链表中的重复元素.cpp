b74a7c9e278c751a7a7951d2e4cce54c3a0504d9
CxtCSDN <724435851@qq.com>
2022-04-16 23:14:37 +0800

    删除链表中的重复元素（日常练习）

---
+++ 删除链表中的重复元素.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) {
+            return head;
+        }
+
+        ListNode* cur = head;
+        while (cur->next) {
+            if (cur->val == cur->next->val) {
+                cur->next = cur->next->next;
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+
+        return head;
+    }
+};
