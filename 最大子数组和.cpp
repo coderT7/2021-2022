b1c2aa5e4362af46e8fdf7feb2c1aeb2cebf1f89
CxtCSDN <724435851@qq.com>
2022-04-17 22:12:57 +0800

    最大子数组和

---
+++ 最大子数组和.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int pre = 0, maxAns = nums[0];
+        for (const auto& x : nums) {
+            pre = max(pre + x, x);
+            maxAns = max(maxAns, pre);
+        }
+        return maxAns;
+    }
+};
