5436d928d03699b17e5ad6d3aa370e7cfddcddfe
CxtCSDN <724435851@qq.com>
2022-04-23 23:09:44 +0800

    力扣水题2236

---
+++ 力扣水题1.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        return root->val == root->left->val + root->right->val;
+    }
+};
