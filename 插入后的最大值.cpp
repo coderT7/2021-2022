bdc0a6aabc513a393d1f83813bdd9f0e16743797
CxtCSDN <724435851@qq.com>
2022-05-26 22:17:52 +0800

    插入后的最大值.cpp

---
+++ 插入后的最大值.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string maxValue(string n, int x) {
+        int l = n.size();
+        if (n[0] == '-') {
+            for (int i = 1; i < l; ++i) {
+                if (n[i] - '0' > x) {
+                    n.insert(i, 1, '0' + x);
+                    return n;
+                }
+            }
+            n.push_back('0' + x);
+            return n;
+        }
+        else {
+            for (int i = 0; i < l; ++i) {
+                if (n[i] - '0' < x) {
+                    n.insert(i, 1, '0' + x);
+                    return n;
+                }
+            }
+            n.push_back('0' + x);
+            return n;
+        }
+
+    }
+};
