255837e7f4a8c8a65cc9779b1ca9faf91da38dc7
CxtCSDN <724435851@qq.com>
2022-09-02 12:01:49 +0800

    每日练习

---
+++ 235.二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=cpp
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q){
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left && right){
+            return root;
+        }
+        if(left){
+            return left;
+        }
+        if(right){
+            return right;
+        }
+        return nullptr;
+    }
+};
+// @lc code=end
+
