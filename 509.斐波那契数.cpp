37bf999387b90a5b76c61bb83aa1656cbe9b1f42
CxtCSDN <724435851@qq.com>
2022-10-01 18:53:42 +0800

    每日练习

---
+++ 509.斐波那契数.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=cpp
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0){
+            return 0;
+        }
+        int a, b, c;
+        a = b = c = 1;
+        while(n-- > 2){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
+// @lc code=end
+
