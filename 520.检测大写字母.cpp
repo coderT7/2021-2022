b6559e6494ca3e98cd831b953550dcb5888c1783
coderT7 <724435851@qq.com>
2022-10-23 23:59:33 +0800

    每日练习

---
+++ 520.检测大写字母.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=520 lang=cpp
+ *
+ * [520] 检测大写字母
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool detectCapitalUse(string word)
+    {
+        // 若第 1 个字母为小写，则需额外判断第 2 个字母是否为小写
+        if (word.size() >= 2 && islower(word[0]) && isupper(word[1]))
+        {
+            return false;
+        }
+
+        // 无论第 1 个字母是否大写，其他字母必须与第 2 个字母的大小写相同
+        for (int i = 2; i < word.size(); ++i)
+        {
+            if (islower(word[i]) ^ islower(word[1]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+                                   // @lc code=end
